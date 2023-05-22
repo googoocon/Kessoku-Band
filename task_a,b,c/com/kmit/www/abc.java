161599b375f1b2b8210b6f238ec92c0f8055fc6c
googoocon <hysm39902@gmail.com>
2023-05-22 22:35:59 +0900

    kessoku band

---
+++ task_a,b,c/com/kmit/www/abc.java
@@ -0,0 +1,23 @@
+package com.kmit.www;
+
+public class abc {
+    public static void main(String[] args) {
+
+        int a = 0;
+        String b = "Hello";
+        int c = 10;
+        double d = 1.4D;
+        String e = null;
+        int f = (int)d;
+        double g = 2.9;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a+c);
+        System.out.println(a+d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+
+    }
+}
